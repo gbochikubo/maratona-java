536462cb952e9de77380275d375df4cd3b47d750
gbochikubo <guilhermeochikubo@gmail.com>
2022-08-31 21:27:36 -0300

    Maratona Java: adicionando busca enum

--- src/main/java/main/enums/TipoCliente.java
+++ src/main/java/main/enums/TipoCliente.java
@@ -5,13 +5,32 @@ package main.enums;
  */
 public enum TipoCliente {
 
-    PESSOA_FISICA(1),
-    PESSOA_JURIDICA(2);
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
 
-    public final int VALUE;
+    private int valor;
+    private String nomeRelatorio;
 
-    TipoCliente(int value){
-        this.VALUE = value;
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio) {
+        for (TipoCliente tipoCliente : values()) {
+            if (tipoCliente.getNomeRelatorio().equals(nomeRelatorio)) {
+                return tipoCliente;
+            }
+        }
+        return null;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
+
+    public int getValor() {
+        return valor;
     }
 
 }
